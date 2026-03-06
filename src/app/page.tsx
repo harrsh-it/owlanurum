1d7fd2e36df61b6b47b8ba88fb979610495c5799
harrsh_it <harshitdharmveer@gmail.com>
2026-03-07 02:33:17 +0530

    Fixing the minor issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 export const revalidate = 60
 import Hero from "@/components/Hero";
+import SubHero from "@/components/SubHero";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <SubHero />
     </>
   );
 }
